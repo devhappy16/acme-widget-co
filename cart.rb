1149525177a153388f47dfcd4fbd3aefb93305eb
pragun16 <pragun.joshi@gmail.com>
2025-07-26 14:19:58 +0545

    add cart class for representation with basic utility methods

---
+++ cart.rb
@@ -0,0 +1,41 @@
+class Cart
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  # returns cart information in hash format
+  def to_h
+    {
+      items: @items.map(&:to_h),
+      items_count: @items.size,
+      subtotal:,
+      total:,
+    }
+  end
+
+  def add(widget_code)
+    widget = Widget.find_by_code(widget_code)
+    raise ArgumentError, "Invalid item code: #{widget_code}" if widget.nil?
+    raise ArgumentError, "Invalid item type: #{widget.class}" unless widget.is_a?(Widget)
+
+    @items << widget
+
+    self
+  end
+
+  def subtotal
+    @items.sum(&:price)
+  end
+
+  def total
+    subtotal
+  end
+
+  def clear
+    @items.clear
+
+    self
+  end
+end
